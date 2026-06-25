32741a207e302ada9982d35f98ae0256f1dceb4e
Tunahn3101 <phamtuananh31012001@gmail.com>
2026-06-25 17:14:44 +0700

    Initial Commit

---
+++ kkphim/kkphimApp.swift
@@ -0,0 +1,17 @@
+//
+//  kkphimApp.swift
+//  kkphim
+//
+//  Created by Tunahn on 25/6/26.
+//
+
+import SwiftUI
+
+@main
+struct kkphimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
